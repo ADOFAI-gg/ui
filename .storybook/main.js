e810e57df1d8293ea8b88ccee7140ba6d7b29bb3
파링 <parangee9706@gmail.com>
2022-04-03 14:14:22 +0900

    feat: story source

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,17 @@
-const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        sourceLoaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
